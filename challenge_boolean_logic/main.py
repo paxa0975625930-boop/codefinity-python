51299262e10b395d632e715351976f2397acc3c4
paxa0975625930-boop <>
2026-05-01 06:18:29 +0000

    Challenge: Boolean Logic

---
+++ challenge_boolean_logic/main.py
@@ -0,0 +1,9 @@
+seasonal = True
+on_sale = False
+selling_well = False
+current_stock = 150
+high_stock_threshold = 100
+overstock_risk = seasonal and (current_stock > high_stock_threshold)
+discount_eligible = not (not seasonal and not on_sale)
+make_discount = overstock_risk or discount_eligible
+print (f'Should the item be discounted? {make_discount}')
