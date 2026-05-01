da03708c3feb60187c88ffea20649075c3185542
paxa0975625930-boop <>
2026-05-01 10:48:31 +0000

    Челендж: Підсумковий Проєкт з Управління Продуктом

---
+++ main.py
@@ -0,0 +1,20 @@
+# Input variables
+days_until_expiration = 5  # Example value
+stock_level = 60  # Example value
+product_type = "Perishable"  # Can be "Perishable" or "Non-Perishable"
+if product_type == 'Perishable':
+    if stock_level > 50:
+        if days_until_expiration <= 3:
+            print ('30% discount applied')
+        if days_until_expiration >=4 and days_until_expiration <= 6:
+            print ("20% discount applied")
+    elif stock_level<=50: 
+        if days_until_expiration > 6:
+            print ("10% discount applied")
+else:
+    print ("No discount available for non-perishable items.")
+
+
+
+
+
